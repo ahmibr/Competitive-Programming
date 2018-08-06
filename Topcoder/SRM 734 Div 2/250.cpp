2f2956d85cbbfdbbd033b4a6ecacbc656f188a17
Ahmed Ibrahim <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2018-08-06 02:18:26 +0200

    Create 250.cpp

---
+++ Topcoder/SRM 734 Div 2/250.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define all(v) (v).begin(),(v).end()
+#define allr(v) (v).rbegin(),(v).rend()
+#define sz(a) int((a).size())
+#define pb push_back
+#define tr(c,i) for(typeof((c).begin()) i = (c).begin(); i != (c).end();i++)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define MP make_pair
+#define EPS      1e-9
+#define F        first
+#define S        second
+#define sc(x)     scanf("%d",&x)
+#define scl(x)     scanf("%I64d",&x)
+#define sq(x)     (x)*(x)
+#define INF (1000000000)
+#define oo (1ll<<60)
+#define PI acos(-1)
+#define POW(x) ((x)*(x))
+
+typedef stringstream ss;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+typedef vector<bool> vb;
+typedef vector<double> vd;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef pair<int, int> ii;
+typedef vector<int> rows;
+typedef vector<rows> matrix;
+
+const int dx[] = { 0, 0, -1, 1, 1, -1, -1, 1 };
+const int dy[] = { -1, 1, 0, 0, -1, 1, -1, 1 };
+
+class TheRoundCityDiv2 {
+public:
+	int find(int r) {
+		
+		int ans = -1; //(0,0) will be counted
+		for(int i=-r;i<=r;++i)
+			for(int j=-r;j<=r;++j)
+				if(i*i+j*j<=r*r)
+					ans++;
+				
+		return ans;
+	}
+};
+
+//<%:testing-code%>
+//Powered by KawigiEdit 2.1.4 (beta) modified by pivanof!
